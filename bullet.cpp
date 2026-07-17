ad351ea3d1f7af143e923c7a48cbc41063ed1d5e
saifistiaknavidnobel <saifistiaknavidnobel@gmail.com>
2026-07-18 00:52:00 +0600

    Refactor Bullet class to use asset loader
    Updated bullet drawing method to use assets and removed texture loading.

--- bullet.cpp
+++ bullet.cpp
@@ -1,8 +1,8 @@
+#include "asset_loader.hpp"
 #include "bullet.hpp"
-#include "global.hpp"
+#include "player.hpp"
 
 Bullet::Bullet(Vector2 intpos,Vector2 targetpos){
-    pos={intpos.x+20*config::scale,intpos.y+16*config::scale};
     dir={targetpos.x-intpos.x, targetpos.y-intpos.y};
     float len=sqrt(dir.x*dir.x+dir.y*dir.y);
     dir={dir.x/len,dir.y/len};
@@ -12,14 +12,10 @@ void Bullet::update(){
     pos.x += velocity.x;
     pos.y += velocity.y;
 }
-void Bullet::load(){
-    bullet_texture=LoadTexture("bullet.png");
-}
+
 void Bullet::draw(Vector2 mouse_pos){
     float rotation=atan2(velocity.y,velocity.x)*RAD2DEG;
-    DrawTextureEx(bullet_texture,pos,rotation,0.5,WHITE);
+    DrawTextureEx(assets.bullet,pos,0,1,WHITE);
 }
-void Bullet::unload(){
-    UnloadTexture(bullet_texture);
-}
+
 
