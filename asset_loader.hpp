b165669666953c6c47b3bdc6071778e6a987efd9
shafin-code1 <ninehshafin077@gmail.com>
2026-07-18 20:31:48 +0600

    Add bullet texture to asset manager

--- asset_loader.hpp
+++ asset_loader.hpp
@@ -4,14 +4,16 @@
 namespace asset{
     constexpr const  char* player="player.png";
     constexpr const char* gun="gun.png";
+    constexpr const char* bullet = "bullet.png";
     constexpr const char* map="map.png";
 }
 class asset_manager{
 public:
 Texture2D player;
 Texture2D gun;
-Texture2D map;
 Texture2D bullet;
+Texture2D map;
 void load();
 void unload();
 };
+extern asset_manager assets;
