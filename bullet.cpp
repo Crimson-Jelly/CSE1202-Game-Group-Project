69968d4b29f57ee1388ab4481b255d90c15edca4
shafin-code1 <ninehshafin077@gmail.com>
2026-07-18 21:06:15 +0600

    Refactor Bullet class for texture handling and position
    Updated bullet position calculation and added texture loading/unloading methods.

--- bullet.cpp
+++ bullet.cpp
@@ -1,9 +1,9 @@
-#include "asset_loader.hpp"
 #include "bullet.hpp"
-#include "player.hpp"
+#include "global.hpp"
 
 Bullet::Bullet(Vector2 intpos,Vector2 targetpos){
-    dir={targetpos.x-intpos.x, targetpos.y-intpos.y};
+    pos={intpos.x+18*config::scale,intpos.y+16*config::scale};
+    dir={targetpos.x-pos.x, targetpos.y-pos.y};
     float len=sqrt(dir.x*dir.x+dir.y*dir.y);
     dir={dir.x/len,dir.y/len};
     velocity={dir.x*speed, dir.y*speed};
@@ -15,7 +15,11 @@ void Bullet::update(){
 
 void Bullet::draw(Vector2 mouse_pos){
     float rotation=atan2(velocity.y,velocity.x)*RAD2DEG;
-    DrawTextureEx(assets.bullet,pos,0,1,WHITE);
+    DrawTextureEx(bullet_texture,pos,rotation,0.5,WHITE);
 }
-
-
+void Bullet::load(){
+    bullet_texture=LoadTexture("bullet.png");
+}
+void Bullet::unload(){
+    UnloadTexture(bullet_texture);
+}
