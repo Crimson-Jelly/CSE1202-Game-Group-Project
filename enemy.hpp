3775e0ea330db2e2276283c35be5f9d136504ab2
shafin-code1 <ninehshafin077@gmail.com>
2026-07-18 21:10:29 +0600

    Add EnemyProjectile class with basic functionality

--- enemy.hpp
+++ enemy.hpp
@@ -1,21 +1,23 @@
-#pragma once
-#include <raylib.h>
+#include "enemyProjectile.hpp"
+#include <cmath>
 
-class Enemy{
-    public:
-    Vector2 pos;
-    float speed=2;
-    int health = 100;
-    float atk_speed=6;
-    float shoot_timer=0;
-    float spawntime=3;
-    bool alive=true;
-    float hitbox=40;
-    static Texture2D enemy_texture;
-
-    Enemy(float mapWidth,float mapHeight);
-    static void load();
-    static void unload();
-    void movement(Vector2 playerPos);
-    void draw(Vector2 playerPos);
-};
+EnemyProjectile::EnemyProjectile(Vector2 intPos,Vector2 targetPos){
+    pos=intPos;
+    dir={targetPos.x-intPos.x, targetPos.y-intPos.y};
+    float len=sqrt(dir.x*dir.x+dir.y*dir.y);
+    dir={dir.x/len,dir.y/len};
+    velocity={dir.x*speed, dir.y*speed};
+}
+void EnemyProjectile::load(){
+    EnemyProjectile_texture=LoadTexture("enemy_projectile.png");
+}
+void EnemyProjectile::unload(){
+    UnloadTexture(EnemyProjectile_texture);
+}
+void EnemyProjectile::update(){
+    pos={pos.x+=velocity.x,pos.y+=velocity.y};
+}
+void EnemyProjectile::draw(){
+    float rotation=atan2(velocity.y,velocity.x)*RAD2DEG;
+    DrawTextureEx(EnemyProjectile_texture,{pos.x-EnemyProjectile_texture.width/2,pos.y-EnemyProjectile_texture.height/2},rotation,1,WHITE);
+}
