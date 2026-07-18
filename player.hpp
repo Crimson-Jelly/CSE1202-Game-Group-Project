a0c79cc368fdf3dbb11f0bd69a66b862ddb02aab
shafin-code1 <ninehshafin077@gmail.com>
2026-07-18 21:14:47 +0600

    Increase player health and add hitbox and scale

--- player.hpp
+++ player.hpp
@@ -5,16 +5,21 @@ class player{
     public:
     Vector2 pos;
     Vector2 dir;
-    int health=100;
+    int health = 240;
+    int hitbox = 60;
     float speed=60*5;
     bool alive=true;
+    float scale=2;
 
     Texture2D player;
     Texture2D gun;
     Texture2D gun_2;
     
     void keyboard_movement(float screenW,float screenH);
+    void heal(int amount);
+void takeDamage(int dmg);
     void load();
+    void reset();
     void draw(Vector2 mouse_pos);
     void unload();
 };
